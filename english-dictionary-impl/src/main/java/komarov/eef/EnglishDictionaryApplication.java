e88fb967bd7136da428b33dc92538e71d916c002
Vasilii Komarov <komarov.vasily884@gmail.com>
2019-02-14 19:39:54 +0400

    Initial commit

---
+++ english-dictionary-impl/src/main/java/komarov/eef/EnglishDictionaryApplication.java
@@ -0,0 +1,17 @@
+package komarov.eef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * English dictionary application starter.
+ *
+ * @author Vasilii Komarov
+ */
+@SpringBootApplication
+public class EnglishDictionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishDictionaryApplication.class);
+    }
+}
